092a29d388efaa43ad4fdc6f0b0e1326e29d1ae2
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-04-28 16:56:19 +0200

    Fix migrations

--- src/boot/drizzle.js
+++ src/boot/drizzle.js
@@ -1,9 +1,14 @@
-import Vue from "vue"
-import store from "../store"
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-import drizzleVuePlugin from "@drizzle/vue-plugin"
-import drizzleOptions from "../drizzleOptions"
+import drizzleVuePlugin from '@drizzle/vue-plugin'
+import drizzleOptions from '../drizzleOptions'
 
-Vue.config.productionTip = false
+// Register Vuex
+Vue.use(Vuex)
 
+// Create and configure your Vuex Store
+const store = new Vuex.Store({ state: {} })
+
+// Register the drizzleVuePlugin
 Vue.use(drizzleVuePlugin, { store, drizzleOptions })
--- src/drizzleOptions.js
+++ src/drizzleOptions.js
@@ -1,20 +1,21 @@
-import Guestbook from "../src/contracts";
+import Guestbook from './contracts/Guestbook.json'
+
 const options = {
-    web3: {
-        block: false,
-        fallback: {
-            type: "ws",
-            url: "ws://127.0.0.1:9545"
-        }
-    },
-    // The contracts to monitor
-    contracts: [Guestbook],
-    events: {
-        Guestbook: ["SignatureAdded"]
-    },
-    polls: {
-        // check accounts ever 15 seconds
-        accounts: 15000
+  web3: {
+    block: false,
+    fallback: {
+      type: 'ws',
+      url: 'ws://127.0.0.1:9545'
     }
-};
-export default options;
+  },
+  // The contracts to monitor
+  contracts: [Guestbook],
+  events: {
+    Guestbook: ['SignatureAdded']
+  },
+  polls: {
+    // check accounts ever 15 seconds
+    accounts: 15000
+  }
+}
+export default options
